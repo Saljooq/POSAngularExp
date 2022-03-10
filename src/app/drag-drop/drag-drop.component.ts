aafd7279e87647fd31f3fa1101a18f97afd768dd
Sal <75107181+Saljooq@users.noreply.github.com>
2022-03-09 21:43:04 -0600

    cleaning up commented out code

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -23,17 +23,6 @@ export class DragDropComponent {
 
   test : string = 'initial comment'
 
-  // drop(event: CdkDragDrop<string[]>){
-  //   if (event.previousContainer === event.container){
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex
-  //     );
-  //   }
 
     drop(event: CdkDragDrop<string[]>){
       if (event.previousContainer === event.container){
